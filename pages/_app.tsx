d10c4bda35157db3ae6279f016b1563f5be65ef1
andrevitalb <santiago_vital@outlook.com>
2022-04-14 17:15:50 -0500

    Added base structure for _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
+import { ComponentType } from "react"
+import "styles/globals.css"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+/**
+ * The App component implicitly wraps every page component. This code runs
+ * client-side and server-side.
+ *
+ * @see https://nextjs.org/docs/advanced-features/custom-app
+ */
+export default function App<Props>({
+	Component,
+	pageProps,
+}: {
+	Component: ComponentType<Props>
+	pageProps: Props
+}) {
+	return (
+				<Component {...pageProps} />
+	)
 }
 
 export default MyApp
