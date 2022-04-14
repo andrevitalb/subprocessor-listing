138d7b57bf5f69e42f7fccdafff25050ac307cd4
andrevitalb <santiago_vital@outlook.com>
2022-04-14 18:01:12 -0500

    Set sample data to use hard-coded uuids

--- lib/sampleData.ts
+++ lib/sampleData.ts
@@ -1,21 +1,20 @@
 import { Subprocessor } from "contexts/SubprocessorsContext"
-import * as uuid from "uuid"
 
 export const mockSubprocessors: Subprocessor[] = [
 	{
-		id: uuid.v4(),
+		id: "024c6466-e1f9-4e7d-8944-aea2c89643b5",
 		name: "Fastly",
 		purpose: "CDN",
 		location: "USA",
 	},
 	{
-		id: uuid.v4(),
+		id: "0230e697-1db1-4be6-ae52-0faf13485c1f",
 		name: "Heroku",
 		purpose: "Hosting",
 		location: "USA",
 	},
 	{
-		id: uuid.v4(),
+		id: "e901fab6-179b-4bee-afb0-f3cc8dedf66c",
 		name: "SendGrid",
 		purpose: "Email Deliverability",
 		location: "USA",
